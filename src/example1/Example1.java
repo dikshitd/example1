410646cf36a22a3c9dff3b7a7e3d1e98e15aa234
dikshitd <dikshit@sheridancollege.ca>
2020-05-28 10:28:18 -0400

    second commit

--- src/example1/Example1.java
+++ src/example1/Example1.java
@@ -17,7 +17,8 @@ public class Example1 {
     public static void main(String[] args) {
         //TODO code application logic here
     System.out.println("test");
-    System.out.println("second testing")
+    System.out.println("second testing");
+            
     }
     
 }
